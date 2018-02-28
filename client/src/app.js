21d54deec35f5e3813061e0fa508187b78be8035
Dave Ellis <dave@ellisnetmail.co.uk>
2018-02-28 16:46:02 +0000

    Added basic code to get data back from rest countries api

--- client/src/app.js
+++ client/src/app.js
@@ -1,4 +1,23 @@
 const app = function(){
+    const url = "https://restcountries.eu/rest/v2/all";
+
+    makeRequest(url, requestComplete);
+}
+
+const makeRequest = function(url, callback){
+    const request = new XMLHttpRequest();
+    request.open('GET', url);
+    request.addEventListener('load', callback);
+    request.send();
+}
+
+const requestComplete = function(){
+    if(this.status !== 200){
+        return;
+    }
+    const jsonString = this.responseText;
+    const countries = JSON.parse(jsonString);
+    console.log(countries);
     
 }
 
